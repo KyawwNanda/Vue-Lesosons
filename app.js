c3287ba2c02a60b57fbd919d15b4b19f3a3e8ad5
Kyaw Nanda <kyawnanda043@gmail.com>
2023-05-05 20:59:09 +0630

    chaper3-ep-1

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title:"Do you know JS",
+            author:"K N D",
+            age:20,
+        }
+    }
+});
+
+app.mount('#app');
